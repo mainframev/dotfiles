05cd34e25c54897df1975d3ff4b1ee71018ddcc2
mainframev <vgenaev@gmail.com>
2022-07-18 11:19:59 +0200

    feat: add wilder setup

---
+++ after/plugin/wilder.rc.lua
@@ -0,0 +1,12 @@
+
+local wilder = require('wilder')
+
+wilder.setup({modes = {':', '/', '?'}})
+
+wilder.set_option('renderer', wilder.popupmenu_renderer({
+  -- highlighter applies highlighting to the candidates  
+  highlighter = wilder.basic_highlighter(),
+  left = {' ', wilder.popupmenu_devicons()},
+  right = {' ', wilder.popupmenu_scrollbar()},
+}))
+
