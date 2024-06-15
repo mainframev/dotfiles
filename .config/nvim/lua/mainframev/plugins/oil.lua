af69e641e62239b2cda28d15c12b94fbf705c963
Victor Genaev <vgenaev@gmail.com>
2024-06-16 01:31:51 +0200

    chore: update oil config

--- .config/nvim/lua/mainframev/plugins/oil.lua
+++ .config/nvim/lua/mainframev/plugins/oil.lua
@@ -1,10 +1,29 @@
 return {
   "stevearc/oil.nvim",
-  opts = {},
   dependencies = { "nvim-tree/nvim-web-devicons" },
   config = function()
     require("oil").setup({
+      -- experimental_watch_for_changes = true,
       default_file_explorer = true,
+      delete_to_trash = true,
+      columns = {
+        "icon",
+        "size",
+      },
+      buf_options = {
+        buflisted = true,
+      },
+      win_options = {
+        signcolumn = "yes",
+        list = true,
+      },
+      view_options = {
+        show_hidden = true,
+        natural_order = true,
+        is_always_hidden = function(name, _)
+          return name == ".." or name == ".git"
+        end,
+      },
     })
   end,
 }
