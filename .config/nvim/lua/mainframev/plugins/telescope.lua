ecfe2d69b60bd18b62653bf38dfd6344a902f033
Victor Genaev <vgenaev@gmail.com>
2024-06-16 01:29:37 +0200

    chore: update telescope config

--- .config/nvim/lua/mainframev/plugins/telescope.lua
+++ .config/nvim/lua/mainframev/plugins/telescope.lua
@@ -6,14 +6,17 @@ return {
     { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
     "nvim-tree/nvim-web-devicons",
     "folke/todo-comments.nvim",
+    "debugloop/telescope-undo.nvim",
+    "gbprod/yanky.nvim",
   },
   config = function()
     local telescope = require("telescope")
+    local builtin = require("telescope.builtin")
     local actions = require("telescope.actions")
     local transform_mod = require("telescope.actions.mt").transform_mod
-
     local trouble = require("trouble")
     local trouble_telescope = require("trouble.sources.telescope")
+
     -- or create your custom action
     local custom_actions = transform_mod({
       open_trouble_qflist = function()
@@ -21,9 +24,24 @@ return {
       end,
     })
 
+    telescope.load_extension("fzf")
+    telescope.load_extension("yank_history")
+
     telescope.setup({
+      extensions = {
+        fzf = {
+          override_generic_sorter = true, -- override the generic sorter
+          override_file_sorter = true, -- override the file sorter
+          case_mode = "smart_case", -- or "ignore_case" or "respect_case"
+        },
+        undo = {
+          side_by_side = true,
+          layout_strategy = "vertical",
+        },
+      },
       defaults = {
-        path_display = { "smart" },
+        path_display = { "absolute" },
+        wrap_results = true,
         mappings = {
           i = {
             ["<C-k>"] = actions.move_selection_previous, -- move to prev result
@@ -35,17 +53,21 @@ return {
       },
     })
 
-    telescope.load_extension("fzf")
-
     -- set keymaps
     local keymap = vim.keymap
 
-    keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Fuzzy find files in cwd" })
-    keymap.set("n", "<leader>fo", "<cmd>Telescope oldfiles<cr>", { desc = "Fuzzy find recent files" })
-    keymap.set("n", "<leader>fw", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
-    keymap.set("n", "<leader>fg", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor in cwd" })
+    keymap.set("n", "<leader>ff", builtin.find_files, {})
+    keymap.set("n", "<leader>fg", builtin.live_grep, {})
+    keymap.set("n", "<leader>fb", builtin.buffers, {})
+    keymap.set("n", "<leader>fg", builtin.git_files, {})
+    keymap.set("n", "<leader>fo", builtin.oldfiles, {})
+    keymap.set("n", "<leader>fd", builtin.diagnostics, {})
+    keymap.set("n", "<leader>fw", builtin.grep_string, {})
+    keymap.set("n", "<leader>gc", builtin.git_commits, {})
+    keymap.set("n", "<leader>km", builtin.keymaps, {})
+
+    keymap.set("n", "<leader>uu", "<cmd>Telescope undo<cr>", { desc = "󰚰 Undo Tree" })
+    keymap.set("n", "<leader>yy", "<cmd>Telescope yank_history<cr>", { desc = "󰮲 Yank history" })
     keymap.set("n", "<leader>ft", "<cmd>TodoTelescope<cr>", { desc = "Find todos" })
-    keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<cr>", { desc = "Find bufffers" })
-    keymap.set("n", "<leader>fd", "<cmd>Telescope diagnostics<cr>", { desc = "Explore diagnostics" })
   end,
 }
