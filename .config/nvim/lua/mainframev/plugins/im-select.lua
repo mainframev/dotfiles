492f53fd5e06778480b6a699b193c28fbe7b7887
Victor Genaev <vgenaev@gmail.com>
2024-06-16 21:19:19 +0200

    feat: add im-select plugin
    It's super annoying always switching back to US layout
    in order to use vim motions, im-select helps to solve
    the problem. For instance, I am typing in russian, after
    hit on ESC it would get back to normal mode with US input source.

---
+++ .config/nvim/lua/mainframev/plugins/im-select.lua
@@ -0,0 +1,11 @@
+return {
+  "keaising/im-select.nvim",
+  config = function()
+    require("im_select").setup({
+      default_im_select = "com.apple.keylayout.USInternational-PC",
+      set_default_events = { "VimEnter", "FocusGained", "InsertLeave", "CmdlineLeave" },
+      set_previous_events = { "InsertEnter" },
+      async_switch_im = true,
+    })
+  end,
+}
