2d2c3bd015d7bd7b60c364f101b2e4cd3f7f8592
Victor Genaev <vgenaev@gmail.com>
2024-08-03 23:56:47 +0200

    feat: add snipe plugin

---
+++ .config/nvim/lua/mainframev/plugins/snipe.lua
@@ -0,0 +1,17 @@
+return {
+  "leath-dub/snipe.nvim",
+  keys = {
+    {
+      "sp",
+      function()
+        require("snipe").open_buffer_menu()
+      end,
+      desc = "Open snipe buffer menu",
+    },
+  },
+  opts = {
+    ui = {
+      position = "topright",
+    },
+  },
+}
