e562205495dd3c56966e369fe53ff3d9134883f2
mainframev <vgenaev@gmail.com>
2024-11-26 23:59:13 +0500

    feat(install): adjust installs with additonal packages

--- .config/install/brew.sh
+++ .config/install/brew.sh
@@ -5,20 +5,30 @@ brew update
 brew upgrade
 
 brew tap homebrew/cask-versions
+brew tap homebrew/bundle
+brew tap homebrew/services
+brew tap microsoft/git
 
 # Install packages
 brew install htop
+brew install curl
+brew install spotify_player
+brew install curlie
 brew install git
+brew install git-delta
+brew install difftastic
 brew install yazi
+brew install diff-so-fancy
 brew install gh
 brew install wget
 brew install yarn
-brew install pyenv                           # python version manager
-brew install readline sqlite3 xz zlib tcl-tk # Python dependencies
+brew install pyenv
+brew install readline sqlite3 xz zlib tcl-tk
 brew install fzf
 brew install clipboard
 brew install cargo-c
 brew install bat
+brew install bat-extras
 brew install prettierd
 brew install tmux
 brew install qemu
@@ -31,14 +41,18 @@ brew install stow
 brew install rust
 brew install ripgrep
 brew install luarocks
+brew install lua-language-server
 brew install fd
 brew install wget
+brew install sketchybar
+brew install zoxide
+brew install jq
 
 sleep 1
 
 echo "Basic brew packages are installed."
 
-# cask packages
+# Cask packages
 brew install --cask spotify
 brew install --cask insomnia
 brew install --cask visual-studio-code
@@ -48,3 +62,12 @@ brew install --cask figma
 brew install --cask zoom
 brew install --cask kitty
 brew install --cask nikitabobko/tap/aerospace
+brew install --cask sf-symbols
+brew install --cask font-sf-pro
+brew install --cask hammerspoon
+brew install --cask font-jetbrains-mono-nerd-font
+brew install --cask font-monaspace-nerd-font
+brew install --cask font-3270-nerd-font
+brew install --cask obsidian
+brew install --cask keycastr
+brew install --cask firefox-developer-edition
