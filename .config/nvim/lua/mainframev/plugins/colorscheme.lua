6aa0b0a83191ae5df73f7c4bdec73652ecd0f2c9
mainframev <vgenaev@gmail.com>
2024-12-21 23:38:20 +0100

    nvim: colorscheme, make telescope borderless

--- .config/nvim/lua/mainframev/plugins/colorscheme.lua
+++ .config/nvim/lua/mainframev/plugins/colorscheme.lua
@@ -3,6 +3,7 @@ return {
   "eldritch-theme/eldritch.nvim",
   config = function()
     local colors = require("eldritch.colors").setup() -- pass in any of the config options as explained above
+
     require("eldritch").setup({
       transparent = false,
       terminal_colors = true,
@@ -27,64 +28,26 @@ return {
       dim_inactive = true,
       lualine_bold = true,
       on_highlights = function(hl, _)
-        local prompt = colors.green
         local none = "NONE"
 
-        hl.TelescopeTitle = {
-          fg = prompt,
-        }
-
-        hl.TelescopeNormal = {
-          bg = colors.bg_dark,
-          fg = colors.fg,
-        }
-
-        hl.TelescopeBorder = {
-          bg = colors.bg_dark,
-          fg = colors.green,
-        }
-
-        hl.TelescopePreviewTitle = {
-          fg = prompt,
-        }
-
-        hl.TelescopePromptBorder = {
-          bg = none,
-          fg = colors.teal,
-        }
-
-        hl.TelecopeResultsTitle = {
-          fg = prompt,
-          bg = none,
-        }
-
-        hl.TelescopeResultsNormal = {
-          bg = none,
-          fg = colors.fg,
-        }
-        --
-        hl.TelescopeMatching = {
-          fg = colors.teal,
-        }
-
-        hl.TelescopePreviewTitle = {
-          fg = prompt,
-        }
-        --
-        hl.TelescopeResultsTitle = {
-          fg = prompt,
-        }
-
-        hl.TelescopePromptNormal = {
-          bg = none,
-          fg = colors.teal,
-        }
-
-        hl.TelescopePromptTitle = {
-          fg = prompt,
-        }
+        -- Telescope (borderless)
+        hl.TelescopePreviewBorder = { fg = colors.bg, bg = colors.bg }
+        hl.TelescopeResultsBorder = { fg = colors.bg, bg = colors.bg }
+        hl.TelescopePromptBorder = { fg = colors.bg, bg = colors.bg }
+        hl.TelescopePromptNormal = { fg = colors.cyan, bg = colors.bg_hightlight }
+        hl.TelescopeResultsTitle = { fg = colors.green, bg = colors.bg_higlight }
+        hl.TelescopePromptPrefix = { fg = colors.pink, bg = none }
+        hl.TelescopePromptCount = { fg = colors.fg_gutter }
+        hl.TelescopeMatching = { fg = colors.cyan }
+        hl.TelescopeSelectionCaret = { fg = colors.magenta2 }
+        hl.TelescopeSelection = { fg = colors.fg, bg = colors.bg_visual }
+        -- hl.TelescopeResultsDiffDelete = { fg = colors.red }
+        -- hl.TelescopeResultsDiffChange = { fg = colors.cyan }
+        hl.TelescopeTitle = { fg = colors.green }
+        hl.TelescopeNormal = { bg = colors.bg, fg = colors.fg }
       end,
     })
+
     vim.cmd("colorscheme eldritch")
 
     vim.api.nvim_set_hl(0, "BufferCurrentSign", { fg = colors.cyan })
@@ -103,21 +66,19 @@ return {
       CmpItemAbbr = { fg = colors.dark3, bg = "NONE" },
       CmpItemKindClass = { fg = colors.orange },
       CmpItemKindConstructor = { fg = colors.purple },
-      -- CmpItemKindFolder = { fg = colors.blue2 },
-      -- CmpItemKindFunction = { fg = colors.blue },
+      CmpItemKindFolder = { fg = colors.blue2 },
+      CmpItemKindFunction = { fg = colors.blue },
       CmpItemKindInterface = { fg = colors.teal, bg = "NONE" },
       CmpItemKindKeyword = { fg = colors.magneta2 },
-      CmpItemKindMethod = { fg = colors.red },
-      CmpItemKindReference = { fg = colors.red },
-      CmpItemKindSnippet = { fg = colors.dark3 },
+      CmpItemKindMethod = { fg = colors.magenta },
+      -- CmpItemKindReference = { fg = colors.red },
+      -- CmpItemKindSnippet = { fg = colors.dark3 },
       CmpItemKindVariable = { fg = colors.cyan, bg = "NONE" },
-      CmpItemKindText = { fg = "LightGrey" },
+      CmpItemKindText = { fg = colors.fg },
       CmpItemMenu = { fg = "#C586C0", bg = "NONE" },
       CmpItemAbbrMatch = { fg = "#569CD6", bg = "NONE" },
       CmpItemAbbrMatchFuzzy = { fg = "#569CD6", bg = "NONE" },
     }
-
-    vim.api.nvim_set_hl(0, "CmpBorderedWindow_FloatBorder", { fg = colors.blue0 })
 
     for group, hl in pairs(highlights) do
       vim.api.nvim_set_hl(0, group, hl)
