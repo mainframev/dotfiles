6f13565af21e4ea566d17380532441a4449e896a
mainframev <vgenaev@gmail.com>
2024-12-22 15:35:48 +0100

    nvim: update keymaps

--- .config/nvim/lua/mainframev/config/keymaps.lua
+++ .config/nvim/lua/mainframev/config/keymaps.lua
@@ -14,13 +14,15 @@ keymap("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights" })
 
 -- Splt and wrap
 keymap("n", "<leader>n", ":so %<CR>", {})
-keymap("n", "<leader>v", ":vsplit<CR>", {})
-keymap("n", "<leader>s", ":split<CR>", {})
-keymap("n", "<leader>t", ":vsplit<CR>:terminal<CR>", {})
-keymap("n", "<leader>w", ":set wrap<CR>", {})
+keymap("n", "<leader>sv", ":vsplit<CR>", {})
+keymap("n", "<leader>sh", ":split<CR>", {})
 
 -- Oil --
 keymap("n", "-", "<CMD>Oil<CR>", { desc = "Open file explorer" })
+
+-- Move lines up and down in visual mode
+keymap("v", "J", ":m '>+1<CR>gv=gv", { desc = "[P]Move line down in visual mode" })
+keymap("v", "K", ":m '<-2<CR>gv=gv", { desc = "[P]Move line up in visual mode" })
 
 -- Open links under cursor in browser with gx
 if vim.fn.has("macunix") == 1 then
@@ -32,3 +34,41 @@ end
 -- Save buffer
 keymap("n", "<leader>w", ":w<CR>", silent)
 keymap("i", "<leader>w", "<ESC>:w<CR>", silent)
+
+-- Replace word globally for the whole buffer
+keymap(
+  "n",
+  "<leader>su",
+  [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]],
+  { desc = "[P]Replace word I'm currently on GLOBALLY" }
+)
+
+-- Replaces the current word with the same word in uppercase, globally
+keymap(
+  "n",
+  "<leader>sU",
+  [[:%s/\<<C-r><C-w>\>/<C-r>=toupper(expand('<cword>'))<CR>/gI<Left><Left><Left>]],
+  { desc = "[P]GLOBALLY replace word I'm on with UPPERCASE" }
+)
+
+-- Replaces the current word with the same word in lowercase, globally
+keymap(
+  "n",
+  "<leader>sL",
+  [[:%s/\<<C-r><C-w>\>/<C-r>=tolower(expand('<cword>'))<CR>/gI<Left><Left><Left>]],
+  { desc = "[P]GLOBALLY replace word I'm on with lowercase" }
+)
+
+-- By default, CTRL-U and CTRL-D scroll by half a screen (50% of the window height)
+-- Scroll by 35% of the window height and keep the cursor centered
+local scroll_percentage = 0.35
+-- Scroll by a percentage of the window height and keep the cursor centered
+vim.keymap.set("n", "<C-d>", function()
+  local lines = math.floor(vim.api.nvim_win_get_height(0) * scroll_percentage)
+  vim.cmd("normal! " .. lines .. "jzz")
+end, { noremap = true, silent = true })
+
+vim.keymap.set("n", "<C-u>", function()
+  local lines = math.floor(vim.api.nvim_win_get_height(0) * scroll_percentage)
+  vim.cmd("normal! " .. lines .. "kzz")
+end, { noremap = true, silent = true })
