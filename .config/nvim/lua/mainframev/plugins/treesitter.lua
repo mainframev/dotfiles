3df5549e61d12bd84f8461059b969ce0cde11216
mainframev <vgenaev@gmail.com>
2025-03-23 00:13:35 +0100

    nvim: add treesitter context

--- .config/nvim/lua/mainframev/plugins/treesitter.lua
+++ .config/nvim/lua/mainframev/plugins/treesitter.lua
@@ -1,44 +1,64 @@
 ---@type LazySpec
 return {
-  "nvim-treesitter/nvim-treesitter",
-  version = false,
-  build = ":TSUpdate",
-  cmd = { "TSUpdateSync", "TSUpdate", "TSInstall" },
-  lazy = vim.fn.argc(-1) == 0, -- load treesitter early when opening a file from the cmdline
-  opts = {
-    highlight = { enable = true },
-    indent = { enable = true },
-    ensure_installed = {
-      "diff",
-      "html",
-      "gitcommit",
-      "gitignore",
-      "css",
-      "javascript",
-      "jsdoc",
-      "json",
-      "jsonc",
-      "lua",
-      "luadoc",
-      "luap",
-      "markdown",
-      "markdown_inline",
-      "printf",
-      "python",
-      "regex",
-      "toml",
-      "tsx",
-      "typescript",
-      "vue",
-      "vim",
-      "vimdoc",
-      "xml",
-      "yaml",
+  {
+    "nvim-treesitter/nvim-treesitter",
+    version = false,
+    build = ":TSUpdate",
+    cmd = { "TSUpdateSync", "TSUpdate", "TSInstall" },
+    lazy = vim.fn.argc(-1) == 0, -- load treesitter early when opening a file from the cmdline
+    opts = {
+      highlight = { enable = true },
+      indent = { enable = true },
+      ensure_installed = {
+        "diff",
+        "html",
+        "gitcommit",
+        "gitignore",
+        "css",
+        "javascript",
+        "jsdoc",
+        "json",
+        "jsonc",
+        "lua",
+        "luadoc",
+        "luap",
+        "markdown",
+        "markdown_inline",
+        "printf",
+        "python",
+        "regex",
+        "toml",
+        "tsx",
+        "typescript",
+        "vue",
+        "vim",
+        "vimdoc",
+        "xml",
+        "yaml",
+      },
     },
+    ---@param opts TSConfig
+    config = function(_, opts)
+      require("nvim-treesitter.configs").setup(opts)
+      vim.treesitter.language.register("markdown", "mdx")
+    end,
   },
-  ---@param opts TSConfig
-  config = function(_, opts)
-    require("nvim-treesitter.configs").setup(opts)
-    vim.treesitter.language.register("markdown", "mdx")
-  end,
+  {
+    "nvim-treesitter/nvim-treesitter-context",
+    opts = function()
+      local tsc = require("treesitter-context")
+      Snacks.toggle({
+        name = "Treesitter Context",
+        get = tsc.enabled,
+        set = function(state)
+          if state then
+            tsc.enable()
+          else
+            tsc.disable()
+          end
+        end,
+      }):map("<leader>up")
+      return { mode = "cursor", max_lines = 3 }
+    end,
+  },
 }
